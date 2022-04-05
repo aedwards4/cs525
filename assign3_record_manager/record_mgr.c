73422fd93eb9e9ed587d3f0f06d339ef20303ebe
Alexis Edwards <101596613+ASP22SCM65E@users.noreply.github.com>
2022-04-05 11:11:38 -0500

    getAttr, setAttr, and helper function added

--- assign3_record_manager/record_mgr.c
+++ assign3_record_manager/record_mgr.c
@@ -1,21 +1,42 @@
-#ifndef RECORD_MGR_H
-#define RECORD_MGR_H
-
+#include "record_mgr.h"
+#include "storage_mgr.h"
+#include "buffer_mgr.h"
 #include "dberror.h"
 #include "expr.h"
 #include "tables.h"
 
 // Bookkeeping for scans
-typedef struct RM_ScanHandle
-{
-	RM_TableData *rel;
-	void *mgmtData;
-} RM_ScanHandle;
+// typedef struct RM_ScanHandle
+// {
+// 	RM_TableData *rel;
+// 	void *mgmtData;
+// } RM_ScanHandle;
 
 // table and manager
-extern RC initRecordManager (void *mgmtData);
-extern RC shutdownRecordManager ();
-extern RC createTable (char *name, Schema *schema);
+extern RC initRecordManager (void *mgmtData) {
+
+	return RC_OK;
+
+}
+
+
+extern RC shutdownRecordManager (){
+
+	return RC_OK;
+
+}
+
+
+extern RC createTable (char *name, Schema *schema){
+
+	// do things
+
+
+	return RC_OK;
+
+}
+
+
 extern RC openTable (RM_TableData *rel, char *name);
 extern RC closeTable (RM_TableData *rel);
 extern RC deleteTable (char *name);
@@ -40,7 +61,115 @@ extern RC freeSchema (Schema *schema);
 // dealing with records and attribute values
 extern RC createRecord (Record **record, Schema *schema);
 extern RC freeRecord (Record *record);
-extern RC getAttr (Record *record, Schema *schema, int attrNum, Value **value);
-extern RC setAttr (Record *record, Schema *schema, int attrNum, Value *value);
 
-#endif // RECORD_MGR_H
+
+
+extern RC getAttr (Record *record, Schema *schema, int attrNum, Value **value){
+
+	// Helper variables
+	int offset = getOffset(schema, attrNum);
+	char *src = record->data + offset;
+	DataType dt = schema->dataTypes[attrNum];
+	*value = (Value*)malloc(sizeof(Value));
+	(*value)->dt = src;
+
+	if (dt == DT_INT) {
+
+		void *dest = value->v.intV
+		memcpy(dest, src, sizeof(int));
+
+	} else if (dt == DT_STRING) {
+
+		char end = '\0';
+		int length = schema->typeLength[attrNum];
+		void* dest = (char*) malloc(length+1);
+		strncopy(dest, src, length);
+		dest[length] = end;
+		(*value)->v.stringV = dest;
+
+	} else if (dt == DT_FLOAT) {
+
+		void *dest = value->v.floatV
+		memcpy(dest, src, sizeof(float));
+
+	} else if (dt == DT_BOOL){
+
+		void *dest = value->v.boolV
+		memcpy(dest, src, sizeof(bool));
+
+	}
+
+	return RC_OK;
+
+}
+
+
+extern RC setAttr (Record *record, Schema *schema, int attrNum, Value *value){
+
+	// Helper variables
+	int offset = getOffset(schema, attrNum);
+	char *dest = record->data + offset;
+	DataType dt = schema->dataTypes[attrNum];
+
+	if (dt == DT_INT) {
+
+		void *src = value->v.intV
+		memcpy(dest, src, sizeof(int));
+
+	} else if (dt == DT_STRING) {
+
+		int length = schema->typeLength[attrNum];
+		char *src = (char*) malloc(length);
+		src = value->v.stringV;
+		memcpy(dest, src, length);
+
+	} else if (dt == DT_FLOAT) {
+
+		void *src = value->v.floatV
+		memcpy(dest, src, sizeof(float));
+
+	} else if (dt == DT_BOOL){
+
+		void *src = value->v.boolV
+		memcpy(dest, src, sizeof(bool));
+
+	}
+
+	return RC_OK;
+}
+
+// Helper function to calculate offset
+int getOffset(Schema *schema, int attrNum) {
+
+	// Helper variables
+	int i = 0;
+	int offset = 0;
+	DataType dt;
+
+	// Calculate offset
+	for (i=0; i < attrNum; i++) {
+
+		dt = schema->dataTypes[i];
+
+		if (dt == DT_INT) {
+
+			offset += sizeof(int);
+
+		} else if (dt == DT_STRING) {
+
+			offset += schema->typeLength[i];
+
+		} else if (dt == DT_FLOAT) {
+
+			offset += sizeof(float);
+
+		} else if (dt == DT_BOOL){
+
+			offset += sizeof(bool);
+
+		}
+	}
+
+	// Return calculated offset
+	return offset;
+}
